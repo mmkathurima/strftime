b3edf56f17745ea75ac93aafa75b430db21562cb
mmkathurima <84718271+mmkathurima@users.noreply.github.com>
2024-03-20 00:25:46 +0300

    Update strftime.js

--- strftime.js
+++ strftime.js
@@ -1,14 +1,17 @@
 //https://cdn.jsdelivr.net/gh/mmkathurima/strftime/strftime.js
-function strftime(date, patternStr) {
-    const twoDigitPad = x => String(x).padStart(2, 0);
-    const dayOfWeekNames = [...Array(7).keys()].map(key => new Date(0, 0, key).toLocaleString('en', { weekday: "long" }));
-    const monthNames = [...Array(12).keys()].map(key => new Date(0, key).toLocaleString('en', { month: 'long' }));
+function strftime(patternStr, date) {
+    const twoDigitPad = x => String(x).padStart(2, '0');
+    const dayOfWeekNames = [...Array(7).keys()].map(key => new Date(0, 0, key)
+        .toLocaleString('en', { weekday: "long" }));
+    const monthNames = [...Array(12).keys()].map(key => new Date(0, key)
+        .toLocaleString('en', { month: 'long' }));
 
-    if (!patternStr) 
+    if (!patternStr)
         patternStr = 'yyyy-MM-dd';
+
     if (!date)
         date = new Date();
-    
+
     const day = date.getDate(),
         month = date.getMonth(),
         year = date.getFullYear(),
@@ -29,27 +32,31 @@ function strftime(date, patternStr) {
         MM = twoDigitPad(M),
         MMMM = monthNames[month],
         MMM = MMMM.substr(0, 3),
-        yyyy = year + "",
-        yy = yyyy.substr(2, 2)
-    ;
-    // checks to see if month name will be used
+        yyyy = String(year),
+        yy = yyyy.substr(2, 2);
+
     patternStr = patternStr.replace('hh', hh)
-      .replace('h', h).replace('HH', HH)
-      .replace('H', hour).replace('mm', mm)
-      .replace('m', minute).replace('ss', ss)
-      .replace('s', second).replace('S', miliseconds)
-      .replace('dd', dd).replace('d', day)
-      .replace('EEEE', EEEE).replace('EEE', EEE)
-      .replace('yyyy', yyyy).replace('yy', yy)
-      .replace('aaa', aaa);
-    if (patternStr.indexOf('MMM') > -1) {
-        patternStr = patternStr
-          .replace('MMMM', MMMM)
-          .replace('MMM', MMM);
-    }
-    else {
-        patternStr = patternStr.replace('MM', MM)
-          .replace('M', M);
-    }
+        .replace('h', String(h))
+        .replace('HH', HH)
+        .replace('H', String(hour))
+        .replace('mm', mm)
+        .replace('m', String(minute))
+        .replace('ss', ss)
+        .replace('s', String(second))
+        .replace('S', String(miliseconds))
+        .replace('dd', dd)
+        .replace('d', String(day))
+        .replace('EEEE', EEEE)
+        .replace('EEE', EEE)
+        .replace('yyyy', yyyy)
+        .replace('yy', yy)
+        .replace('aaa', aaa);
+
+    if (patternStr.indexOf('MMM') > -1)
+        patternStr = patternStr.replace('MMMM', MMMM).replace('MMM', MMM);
+    else
+        patternStr = patternStr.replace('MM', MM).replace('M', String(M));
+
     return patternStr;
 }
+
