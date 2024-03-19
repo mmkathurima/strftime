f0ac364b6165fa72d9d89b04df150ab5dc4482a1
mmkathurima <84718271+mmkathurima@users.noreply.github.com>
2024-03-20 00:36:09 +0300

    Added C like date formatting function.

--- strftime.js
+++ strftime.js
@@ -1,5 +1,5 @@
 //https://cdn.jsdelivr.net/gh/mmkathurima/strftime/strftime.js
-function strftime(patternStr, date) {
+function formatDate(patternStr, date) {
     const twoDigitPad = x => String(x).padStart(2, '0');
     const dayOfWeekNames = [...Array(7).keys()].map(key => new Date(0, 0, key)
         .toLocaleString('en', { weekday: "long" }));
@@ -60,3 +60,70 @@ function strftime(patternStr, date) {
     return patternStr;
 }
 
+function strftime(sFormat, date) {
+    const nDay = date.getDay();
+    const nDate = date.getDate();
+    const nMonth = date.getMonth();
+    const nYear = date.getFullYear();
+    const nHour = date.getHours();
+    const nTime = date.getTime();
+    const aDays = [...Array(7).keys()].map(key =>
+        new Date(0, 0, key).toLocaleString('en', { weekday: 'long' }));
+    const aMonths = [...Array(12).keys()].map(key =>
+        new Date(0, key).toLocaleString('en', { month: 'long' }));
+    const aDayCount = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334];
+    const isLeapYear = () => (nYear % 4 === 0 && nYear % 100 !== 0) || nYear % 400 === 0;
+    const getThursday = () => {
+        const target = new Date(date);
+        target.setDate(nDate - ((nDay + 6) % 7) + 3);
+        return target;
+    };
+    const zeroPad = (nNum, nPad) => ((Math.pow(10, nPad) + nNum) + '').slice(1);
+
+    return sFormat.replace(/%[a-z]+\b/gi, (sMatch) => {
+        return (({
+            '%a': aDays[nDay].slice(0, 3),
+            '%A': aDays[nDay],
+            '%b': aMonths[nMonth].slice(0, 3),
+            '%B': aMonths[nMonth],
+            '%c': date.toUTCString().replace(',', ''),
+            '%C': Math.floor(nYear / 100),
+            '%d': zeroPad(nDate, 2),
+            '%e': nDate,
+            '%F': (new Date(nTime - (date.getTimezoneOffset() * 60000))).toISOString().slice(0, 10),
+            '%G': getThursday().getFullYear(),
+            '%g': (getThursday().getFullYear() + '').slice(2),
+            '%H': zeroPad(nHour, 2),
+            '%I': zeroPad((nHour + 11) % 12 + 1, 2),
+            '%j': zeroPad(aDayCount[nMonth] + nDate + ((nMonth > 1 && isLeapYear()) ? 1 : 0), 3),
+            '%k': nHour,
+            '%l': (nHour + 11) % 12 + 1,
+            '%m': zeroPad(nMonth + 1, 2),
+            '%n': nMonth + 1,
+            '%M': zeroPad(date.getMinutes(), 2),
+            '%p': (nHour < 12) ? 'AM' : 'PM',
+            '%P': (nHour < 12) ? 'am' : 'pm',
+            '%s': Math.round(nTime / 1000),
+            '%S': zeroPad(date.getSeconds(), 2),
+            '%u': nDay || 7,
+            '%V': (() => {
+                const target = getThursday();
+                const n1stThu = target.valueOf();
+                target.setMonth(0, 1);
+                const nJan1 = target.getDay();
+                if (nJan1 !== 4) target.setMonth(0, 1 + ((4 - nJan1) + 7) % 7);
+                return zeroPad(1 + Math.ceil((n1stThu - target) / 604800000), 2);
+            })(),
+            '%w': nDay,
+            '%x': date.toLocaleDateString(),
+            '%X': date.toLocaleTimeString(),
+            '%y': (nYear + '').slice(2),
+            '%Y': nYear,
+            '%z': date.toTimeString().replace(/.+GMT([+-]\d+).+/, '$1'),
+            '%Z': date.toTimeString().replace(/.+\((.+?)\)$/, '$1'),
+            '%Zs': new Intl.DateTimeFormat('default', {
+                timeZoneName: 'short',
+            }).formatToParts(date).find((oPart) => oPart.type === 'timeZoneName')?.value,
+        }[sMatch] || '') + '') || sMatch;
+    });
+}
